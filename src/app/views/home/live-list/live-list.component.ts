b2c3a7a7d9ff802993dada7ac9baf199cc4cff95
jefferson <jeff.keiryw@gmail.com>
2022-04-01 16:06:54 -0300

    pegando dados das live de uma api externa

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Live } from 'src/app/shared/model/live.model';
+import { LiveService } from 'src/app/shared/service/live.service';
 
 @Component({
   selector: 'app-live-list',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  livesPrevious: Live[];
+  livesNext: Live[];
+
+  constructor(
+    public liveService: LiveService
+  ) { }
 
   ngOnInit(): void {
+    this.getLives();
+  }
+
+  getLives(){
+    this.liveService.getLiveswithFlag('previous').subscribe(data=>{
+      this.livesPrevious = data.content;
+    });
+
+    this.liveService.getLiveswithFlag('next').subscribe(data=>{
+      this.livesNext = data.content;
+    });
   }
 
 }
